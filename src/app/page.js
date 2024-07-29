e481b71473a11fcfa9e2bdce7409ac557b68fcd0
jiysait <jiysait@gmail.com>
2024-07-29 08:57:47 -0600

    three basic functionality check

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,33 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
-import modelPath from "/chestbox.glb";
+"use client";
 
-function Model(props) {
-  const gltf = useGLTF(modelPath);
-  return <primitive {...props} object={gltf.scene} />;
-}
+import React from "react";
+// import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+// import { useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { useGLTF } from "@react-three/drei";
+// import modelPath from "../../public/chestbox.glb";
+
+// function Model(props) {
+//   // const gltf = useLoader(GLTFLoader, modelPath);
+//   const gltf = useGLTF(modelPath);
+//   return <primitive {...props} object={gltf.scene} />;
+// }
 
 export default function Home() {
   return (
+    // <Canvas>
+    //   <ambientLight />
+    //   <Suspense>
+    //     <Model />
+    //   </Suspense>
+    // </Canvas>
     <Canvas>
-      <ambientLight />
-      <Suspense>
-        <Model />
-      </Suspense>
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <sphereGeometry />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
     </Canvas>
   );
 }
