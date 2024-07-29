7fa631c863dd95e916f44f3fa7b3b0c728461491
jiysait <jiysait@gmail.com>
2024-07-29 10:41:38 -0600

    applied drei library

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,14 @@
 import React from "react";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { useGLTF } from "@react-three/drei";
+// import { useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useGLTF } from "@react-three/drei";
 // import modelPath from "../../public/chestbox.glb";
 
 function Model(props) {
-  const gltf = useLoader(GLTFLoader, "../../public/chestbox.glb");
-  // const gltf = useGLTF(modelPath);
+  // const gltf = useLoader(GLTFLoader, "../../public/chestbox.glb");
+  const gltf = useGLTF(modelPath);
   return <primitive {...props} object={gltf.scene} />;
 }
 
