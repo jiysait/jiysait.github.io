a7140cd3edcac826631efae46ec38b7883ba5248
jiysait <jiysait@gmail.com>
2024-07-29 09:18:41 -0600

    custom model deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,33 @@
 "use client";
 
 import React from "react";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-// import { useLoader } from "@react-three/fiber";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // import { useGLTF } from "@react-three/drei";
 // import modelPath from "../../public/chestbox.glb";
 
-// function Model(props) {
-//   // const gltf = useLoader(GLTFLoader, modelPath);
-//   const gltf = useGLTF(modelPath);
-//   return <primitive {...props} object={gltf.scene} />;
-// }
+function Model(props) {
+  const gltf = useLoader(GLTFLoader, "../../public/chestbox.glb");
+  // const gltf = useGLTF(modelPath);
+  return <primitive {...props} object={gltf.scene} />;
+}
 
 export default function Home() {
   return (
-    // <Canvas>
-    //   <ambientLight />
-    //   <Suspense>
-    //     <Model />
-    //   </Suspense>
-    // </Canvas>
     <Canvas>
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <sphereGeometry />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
+      <ambientLight />
+      <Suspense>
+        <Model />
+      </Suspense>
     </Canvas>
+    // <Canvas>
+    //   <pointLight position={[10, 10, 10]} />
+    //   <mesh>
+    //     <sphereGeometry />
+    //     <meshStandardMaterial color="hotpink" />
+    //   </mesh>
+    // </Canvas>
   );
 }
