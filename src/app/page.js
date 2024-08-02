f76436ad8765ba6643a92621a07f3fb16a7212df
jiysait <jiysait@gmail.com>
2024-08-02 14:21:49 -0600

    static assets in public test

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,35 @@
 "use client";
 
 import React from "react";
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import Image from "next/image";
+// import { Suspense } from "react";
+// import { Canvas } from "@react-three/fiber";
 // import { useLoader } from "@react-three/fiber";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useGLTF } from "@react-three/drei";
+// import { useGLTF } from "@react-three/drei";
 // import modelPath from "../../public/chestbox.glb";
 
-function Model(props) {
-  // const gltf = useLoader(GLTFLoader, "../../public/chestbox.glb");
-  // const gltf = useGLTF(modelPath);
-  const gltf = useGLTF("../../public/chestbox.glb");
-  return <primitive {...props} object={gltf.scene} />;
-}
+// function Model(props) {
+//   // const gltf = useLoader(GLTFLoader, "../../public/chestbox.glb");
+//   // const gltf = useGLTF(modelPath);
+//   const gltf = useGLTF("../../public/chestbox.glb");
+//   return <primitive {...props} object={gltf.scene} />;
+// }
 
 export default function Home() {
   return (
-    <Canvas>
-      <ambientLight />
-      <Suspense>
-        <Model />
-      </Suspense>
-    </Canvas>
+    <Image
+      src="/fox.jpeg"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+    // <Canvas>
+    //   <ambientLight />
+    //   <Suspense>
+    //     <Model />
+    //   </Suspense>
+    // </Canvas>
     // <Canvas>
     //   <pointLight position={[10, 10, 10]} />
     //   <mesh>
