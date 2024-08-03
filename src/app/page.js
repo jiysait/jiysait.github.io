a01b013e1af4aa78099ea1745a4a9f16b5ad2d8f
jiysait <jiysait@gmail.com>
2024-08-03 17:27:11 -0600

    image test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div class="w-screen h-screen">
-      <></>
+      <Image
+        src="/background_with_tulip.png"
+        width={500}
+        height={500}
+        alt="Jongil Yoon"
+      />
     </div>
   );
 }
