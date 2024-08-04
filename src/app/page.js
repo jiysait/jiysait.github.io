5655195bfbd40483bf980b016c88d8f881f6f2ff
jiysait <jiysait@gmail.com>
2024-08-03 23:22:10 -0600

    class target anime

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,18 @@
+import { useEffect } from "react";
+
 import Image from "next/image";
 import Anime from "react-anime";
-import * as React from "react";
 
-const MyAnime = (props) => (
-  <Anime
-    easing="easeOutQuad"
-    duration={1500}
-    loop={true}
-    svg
-    component="g"
-    delay={(el, index) => index * 200}
-    direction="alternate"
-    strokeDashoffset={(el) => {
-      var pathLength = "0";
-      for (var key in el.children) {
-        // let child: SVGPathElement = el.children[key] as SVGPathElement;
-        let child = el.children[key];
-        if (child.getTotalLength) {
-          pathLength = child.getTotalLength().toString();
-          el.setAttribute("stroke-dasharray", pathLength);
-        }
-      }
-      return [pathLength, 0];
-    }}
-    {...props}
-  />
-);
+const MyAnime = (props) =>
+  anime({
+    targets: ".st0, .st1",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "cubicBezier(.5, .05, .1, .3)",
+    duration: 1000,
+    delay: function (el, i) {
+      return i * 250;
+    },
+  });
 
 const SvgComponent = (props) => (
   <svg
@@ -35,54 +22,54 @@ const SvgComponent = (props) => (
     viewBox="0 0 210 297"
     {...props}
   >
-    <MyAnime>
-      <path
-        d="M117.48 112.715 85.51 95.697l-32.144 16.688L59.67 76.72 33.867 51.308l35.867-5.024L85.93 13.889l15.861 32.56 35.814 5.392-26.065 25.146z"
-        style={{
-          fill: "#000",
-          strokeWidth: 0.264999,
-        }}
-      />
-    </MyAnime>
-    <MyAnime>
-      <path
-        d="M82.485 98.256h5.082v109.297h-5.082z"
-        style={{
-          fill: "#000",
-          strokeWidth: 0.264999,
-        }}
-      />
-    </MyAnime>
-    <MyAnime>
-      <ellipse
-        cx={-15.927}
-        cy={220.237}
-        rx={30.917}
-        ry={12.946}
-        style={{
-          fill: "#000",
-          strokeWidth: 0.264999,
-        }}
-        transform="rotate(-35.907)"
-      />
-    </MyAnime>
-    <MyAnime>
-      <ellipse
-        cx={-153.003}
-        cy={121.62}
-        rx={30.917}
-        ry={12.946}
-        style={{
-          fill: "#000",
-          strokeWidth: 0.264999,
-        }}
-        transform="scale(-1 1) rotate(-35.907)"
-      />
-    </MyAnime>
+    <path
+      class="st0"
+      d="M117.48 112.715 85.51 95.697l-32.144 16.688L59.67 76.72 33.867 51.308l35.867-5.024L85.93 13.889l15.861 32.56 35.814 5.392-26.065 25.146z"
+      style={{
+        fill: "#000",
+        strokeWidth: 0.264999,
+      }}
+    />
+    <path
+      class="st0"
+      d="M82.485 98.256h5.082v109.297h-5.082z"
+      style={{
+        fill: "#000",
+        strokeWidth: 0.264999,
+      }}
+    />
+    <ellipse
+      class="st0"
+      cx={-15.927}
+      cy={220.237}
+      rx={30.917}
+      ry={12.946}
+      style={{
+        fill: "#000",
+        strokeWidth: 0.264999,
+      }}
+      transform="rotate(-35.907)"
+    />
+    <ellipse
+      class="st0"
+      cx={-153.003}
+      cy={121.62}
+      rx={30.917}
+      ry={12.946}
+      style={{
+        fill: "#000",
+        strokeWidth: 0.264999,
+      }}
+      transform="scale(-1 1) rotate(-35.907)"
+    />
   </svg>
 );
 
 export default function Home() {
+  useEffect(() => {
+    MyAnime();
+  }, []);
+
   return (
     <div class="w-screen h-screen bg-white">
       {/* <Image
