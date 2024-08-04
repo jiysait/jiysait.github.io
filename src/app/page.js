50de8503d5c5579b052096f4b2ad80869551b30b
jiysait <jiysait@gmail.com>
2024-08-04 07:29:41 -0600

    react-anime test

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,23 @@
-"use client";
-import React, { useEffect, useState } from "react";
-
 import Image from "next/image";
-// import Anime from "react-anime";
-import anime from "animejs";
+import Anime, { anime } from "react-anime";
+
+let colors = ["blue", "green", "red"];
 
 export default function Home() {
-  const [animationRef, setAnimationRef] = useState();
-
-  useEffect(() => {
-    setAnimationRef(
-      anime({
-        targets: ["path"],
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: "easeInOutSine",
-        duration: 1500,
-        delay: function (el, i) {
-          return i * 250;
-        },
-        direction: "alternate",
-      })
-    );
-  }, []);
-
   return (
     <div className="w-screen h-screen bg-white">
-      <Image src="/curve.svg" alt="curve" width={200} height={200} priority />
+      <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
+        {colors.map((color, i) => (
+          <div key={i} className={color} />
+        ))}
+      </Anime>
+      {/* <Image src="/curve.svg" alt="curve" width={200} height={200} priority /> */}
       {/* <Image
         src="/background_with_tulip.png"
         width={500}
         height={500}
         alt="Jongil Yoon"
       /> */}
-      {/* <SvgComponent width={180} height={370} /> */}
     </div>
   );
 }
