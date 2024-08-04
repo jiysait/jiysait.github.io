ded99a3b356af17fc9b8b00887104fa81b0b85f9
jiysait <jiysait@gmail.com>
2024-08-03 21:19:19 -0600

    happyness

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div class="w-screen h-screen bg-white">
       <Image
+        src="/happyness.svg"
+        alt="happyness"
+        width={180}
+        height={37}
+        priority
+      />
+      <Image
         src="/background_with_tulip.png"
         width={500}
         height={500}
