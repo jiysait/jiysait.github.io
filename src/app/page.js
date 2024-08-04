0eafa32170c94f9082ad64c0050825fb5fc18bc0
jiysait <jiysait@gmail.com>
2024-08-04 07:39:37 -0600

    react-anime test

--- src/app/page.js
+++ src/app/page.js
@@ -6,19 +6,26 @@ let colors = ["blue", "green", "red"];
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-white">
-      <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
-        {colors.map((color, i) => (
-          <div key={i} className={color} />
-        ))}
-      </Anime>
-      {/* <Image src="/curve.svg" alt="curve" width={200} height={200} priority /> */}
-      {/* <Image
-        src="/background_with_tulip.png"
-        width={500}
-        height={500}
-        alt="Jongil Yoon"
-      /> */}
-    </div>
+    <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
+      {colors.map((color, i) => (
+        <div key={i} className={color} />
+      ))}
+    </Anime>
   );
+  // return (
+  //   <div className="w-screen h-screen bg-white">
+  //     <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
+  //       {colors.map((color, i) => (
+  //         <div key={i} className={color} />
+  //       ))}
+  //     </Anime>
+  //     {/* <Image src="/curve.svg" alt="curve" width={200} height={200} priority /> */}
+  //     {/* <Image
+  //       src="/background_with_tulip.png"
+  //       width={500}
+  //       height={500}
+  //       alt="Jongil Yoon"
+  //     /> */}
+  //   </div>
+  // );
 }
