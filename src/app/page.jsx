b461775bb895da524edeea255a99141aaf6ce7f4
jiysait <jiysait@gmail.com>
2024-08-05 17:19:25 -0600

    jsx test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,10 @@
-"use client";
-
-import TestAnime1 from "@/components/test-anime-1";
+// import TestAnime1 from "@/components/test-anime-1";
 import TestImage from "@/components/test-image";
 
 export default function Home() {
   return (
     <>
-      <TestAnime1 />
+      {/* <TestAnime1 /> */}
       <TestImage />
     </>
   );
