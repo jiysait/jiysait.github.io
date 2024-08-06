7bb843c53ea68d965723c12766404cff145c892d
jiysait <jiysait@gmail.com>
2024-08-05 19:19:35 -0600

    svg test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
-// import TestImage from "@/components/test-image";
+import TestAnimeSVG from "@/components/test-anime-svg";
 import TestReactAnime from "@/components/test-react-anime";
+import TestImage from "@/components/test-image";
 
 export default function Home() {
   return (
     <>
-      <TestReactAnime />
+      {/* <TestReactAnime /> */}
       {/* <TestImage /> */}
+      <TestAnimeSVG />
     </>
   );
 }
