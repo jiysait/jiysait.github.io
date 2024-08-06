5da6e4c76c7aec06af69c66da85724c030d20ad1
jiysait <jiysait@gmail.com>
2024-08-05 18:02:29 -0600

    include react

--- src/components/test-anime-1.jsx
+++ src/components/test-anime-1.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import anime from "anime";
 
 export default function TestAnime1() {
