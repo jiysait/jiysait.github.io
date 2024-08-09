d30ded963a8e3f20d689f90f3d2ea68515f4a738
jiysait <jiysait@gmail.com>
2024-08-09 11:32:18 -0600

    plain svg test

--- src/components/test-anime-svg.jsx
+++ src/components/test-anime-svg.jsx
@@ -12,7 +12,7 @@ export default function TestAnimeSVG() {
         viewBox="0 0 1122.5197 793.70081"
         // preserveAspectRatio="xMidYMax meet"
       >
-        <Anime
+        {/* <Anime
           svg
           // strokeDashoffset={[anime.setDashoffset, 0]}
           strokeDashoffset={(el) => {
@@ -31,7 +31,7 @@ export default function TestAnimeSVG() {
           easing="cubicBezier(.5, .05, .1, .3)"
           duration={1000}
           delay={(el, index) => index * 240}
-        >
+        > */}
           <path
             style={{
               fill: "none",
@@ -45,7 +45,7 @@ export default function TestAnimeSVG() {
             d="m 206.06972,284.50917 c 212.71714,-18.61275 434.74065,124.97131 434.74065,124.97131 0,0 136.93665,115.66494 -111.6765,160.86733 -248.61315,45.20239 -247.28366,-90.40478 -247.28366,-90.40478 0,0 -313.75778,-87.74581 53.17928,-118.3239 366.93706,-30.57809 132.9482,127.63028 132.9482,127.63028"
             id="path1"
           />
-        </Anime>
+        {/* </Anime> */}
       </svg>
     </div>
   );
