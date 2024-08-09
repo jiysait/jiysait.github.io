882e01f14e05412be35324fda6c8135a9babebbe
jiysait <jiysait@gmail.com>
2024-08-09 11:35:07 -0600

    anime component test

--- src/components/test-anime-svg.jsx
+++ src/components/test-anime-svg.jsx
@@ -12,26 +12,26 @@ export default function TestAnimeSVG() {
         viewBox="0 0 1122.5197 793.70081"
         // preserveAspectRatio="xMidYMax meet"
       >
-        {/* <Anime
+        <Anime
           svg
           // strokeDashoffset={[anime.setDashoffset, 0]}
-          strokeDashoffset={(el) => {
-            console.log(el);
-            var pathLength = "0";
-            for (var key in el.children) {
-              // let child: SVGPathElement = el.children[key] as SVGPathElement;
-              let child = el.children[key];
-              if (child.getTotalLength) {
-                pathLength = child.getTotalLength().toString();
-                el.setAttribute("stroke-dasharray", pathLength);
-              }
-            }
-            return [pathLength, 0];
-          }}
-          easing="cubicBezier(.5, .05, .1, .3)"
-          duration={1000}
-          delay={(el, index) => index * 240}
-        > */}
+          // strokeDashoffset={(el) => {
+          //   console.log(el);
+          //   var pathLength = "0";
+          //   for (var key in el.children) {
+          //     // let child: SVGPathElement = el.children[key] as SVGPathElement;
+          //     let child = el.children[key];
+          //     if (child.getTotalLength) {
+          //       pathLength = child.getTotalLength().toString();
+          //       el.setAttribute("stroke-dasharray", pathLength);
+          //     }
+          //   }
+          //   return [pathLength, 0];
+          // }}
+          // easing="cubicBezier(.5, .05, .1, .3)"
+          // duration={1000}
+          // delay={(el, index) => index * 240}
+        >
           <path
             style={{
               fill: "none",
@@ -45,7 +45,7 @@ export default function TestAnimeSVG() {
             d="m 206.06972,284.50917 c 212.71714,-18.61275 434.74065,124.97131 434.74065,124.97131 0,0 136.93665,115.66494 -111.6765,160.86733 -248.61315,45.20239 -247.28366,-90.40478 -247.28366,-90.40478 0,0 -313.75778,-87.74581 53.17928,-118.3239 366.93706,-30.57809 132.9482,127.63028 132.9482,127.63028"
             id="path1"
           />
-        {/* </Anime> */}
+        </Anime>
       </svg>
     </div>
   );
