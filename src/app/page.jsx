07207004910729fe0335f68efd9a2998282ac2ea
jiysait <jiysait@gmail.com>
2024-08-10 12:22:37 -0600

    anime example test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
-import TestAnimeSVG from "@/components/test-anime-svg";
-import TestReactAnime from "@/components/test-react-anime";
-import TestImage from "@/components/test-image";
+// import TestAnimeSVG from "@/components/test-anime-svg";
+// import TestReactAnime from "@/components/test-react-anime";
+// import TestImage from "@/components/test-image";
+import TestExample from "@/components/test-example";
 
 export default function Home() {
   return (
     <>
       {/* <TestReactAnime /> */}
       {/* <TestImage /> */}
-      <TestAnimeSVG />
+      {/* <TestAnimeSVG /> */}
+      <TestExample />
     </>
   );
 }
