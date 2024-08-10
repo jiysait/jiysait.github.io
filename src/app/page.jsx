e6501741b40a7ab610fcdeb4df1b789f2efacd8a
jiysait <jiysait@gmail.com>
2024-08-10 12:25:03 -0600

    changed example

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,15 @@
-// import TestAnimeSVG from "@/components/test-anime-svg";
 // import TestReactAnime from "@/components/test-react-anime";
 // import TestImage from "@/components/test-image";
-import TestExample from "@/components/test-example";
+import TestAnimeSVG from "@/components/test-anime-svg";
+// import TestExample from "@/components/test-example";
 
 export default function Home() {
   return (
     <>
       {/* <TestReactAnime /> */}
       {/* <TestImage /> */}
-      {/* <TestAnimeSVG /> */}
-      <TestExample />
+      <TestAnimeSVG />
+      {/* <TestExample /> */}
     </>
   );
 }
