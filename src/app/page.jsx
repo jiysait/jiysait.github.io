e2b90dc9f952d9c9d24744e24d0a1642052e79e8
jiysait <jiysait@gmail.com>
2024-08-17 13:38:51 -0600

    simplify issue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,12 @@
-// import TestUseEffectAnimeJS from "@/components/test-useeffect-animejs";
-
 // import TestBrushPath from "@/components/test-brush-path";
-import TestFill from "@/components/test-fill";
+
+import TestLogo from "@/components/test-logo";
 
 export default function Home() {
   return (
     <>
-      {/* <TestUseEffectAnimeJS /> */}
       {/* <TestBrushPath /> */}
-      <TestFill />
+      <TestLogo />
     </>
   );
 }
