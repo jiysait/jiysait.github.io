89b0bfbd2c1e0c9d8105023366bf0d0d93c3f83a
jiysait <jiysait@gmail.com>
2024-08-17 09:33:35 -0600

    corrected css

--- src/components/test-fill.jsx
+++ src/components/test-fill.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import "../custom.css";
+import "../app/custom.css";
 
 export default function TestFill() {
   /////////////////////////////////
