1fa42cef8635002eac85af8d4f7fef94818e1532
jiysait <jiysait@gmail.com>
2024-08-17 22:50:53 -0600

    clippath

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 // import TestBrushPath from "@/components/test-brush-path";
 
+import TestClipPath from "@/components/test-clippath";
 import TestLogo from "@/components/test-logo";
 
 export default function Home() {
   return (
     <>
       {/* <TestBrushPath /> */}
-      <TestLogo />
+      {/* <TestLogo /> */}
+      <TestClipPath />
     </>
   );
 }
