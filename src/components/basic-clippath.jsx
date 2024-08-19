3c0061257f32432fcc4b83c7f212694dcd5bf474
jiysait <jiysait@gmail.com>
2024-08-19 10:44:51 -0600

    example origin

--- src/components/basic-clippath.jsx
+++ src/components/basic-clippath.jsx
@@ -6,16 +6,16 @@ import "../app/custom.css";
 export default function BasicClipPath() {
   return (
     <div className={`bg-white`}>
-      {/* <clipPath id="myClip">
+      <clipPath id="myClip">
         <circle cx="40" cy="35" r="35" />
-      </clipPath> */}
+      </clipPath>
       <svg viewBox="0 0 100 100">
         <path
           id="heart"
           d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
         />
       </svg>
-      {/* <use clip-path="url(#myClip)" href="#heart" fill="red" /> */}
+      <use clip-path="url(#myClip)" href="#heart" fill="red" />
     </div>
   );
 }
