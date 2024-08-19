d7547e094b5fb323645d89b9ad8382593552c163
jiysait <jiysait@gmail.com>
2024-08-19 10:18:20 -0600

    use tag

--- src/components/basic-clippath.jsx
+++ src/components/basic-clippath.jsx
@@ -14,6 +14,7 @@ export default function BasicClipPath() {
           d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
         />
       </svg>
+      <use clip-path="url(#myClip)" href="#heart" fill="red" />
     </div>
   );
 }
