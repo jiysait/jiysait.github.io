86e504f7c4f1219f5678e2c2fba3780e1b3cc616
jiysait <jiysait@gmail.com>
2024-08-19 10:06:32 -0600

    bg white

--- src/components/basic-clippath.jsx
+++ src/components/basic-clippath.jsx
@@ -4,11 +4,13 @@ import React from "react";
 
 export default function BasicClipPath() {
   return (
-    <svg viewBox="0 0 100 100">
-      <path
-        id="heart"
-        d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
-      />
-    </svg>
+    <div className={`bg-white`}>
+      <svg viewBox="0 0 100 100">
+        <path
+          id="heart"
+          d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
+        />
+      </svg>
+    </div>
   );
 }
