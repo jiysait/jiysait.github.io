f70004f28219fdfb0897e10b91657a7e7466520b
jiysait <jiysait@gmail.com>
2024-08-24 13:35:52 -0600

    svg tag corrected

--- src/components/basic-clippath.jsx
+++ src/components/basic-clippath.jsx
@@ -5,17 +5,15 @@ import "../app/custom.css";
 
 export default function BasicClipPath() {
   return (
-    <div className={`bg-white`}>
+    <svg viewBox="0 0 100 100">
       <clipPath id="myClip">
         <circle cx="40" cy="35" r="35" />
       </clipPath>
-      <svg viewBox="0 0 100 100">
-        <path
-          id="heart"
-          d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
-        />
-      </svg>
+      <path
+        id="heart"
+        d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"
+      />
       <use clip-path="url(#myClip)" href="#heart" fill="red" />
-    </div>
+    </svg>
   );
 }
