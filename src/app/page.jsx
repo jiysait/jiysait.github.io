06c692342136289ffb960a9f1f13422f83104c98
jiysait <jiysait@gmail.com>
2024-09-14 22:41:45 -0600

    hello there!

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,9 @@
-// import TestBrushPath from "@/components/test-brush-path";
-
-import BasicBrushDrawing from "@/components/basic-brush-drawing";
-import BasicClipPath from "@/components/basic-clippath";
-import TestClipPath from "@/components/test-clippath";
-import TestLogo from "@/components/test-logo";
+import Hello from "@/components/hello";
 
 export default function Home() {
   return (
     <>
-      {/* <TestBrushPath /> */}
-      {/* <TestLogo /> */}
-      {/* <TestClipPath /> */}
-      {/* <BasicClipPath /> */}
-      <BasicBrushDrawing />
+      <Hello />
     </>
   );
 }
